4f2c802325f8bf4394824eb94ba20c950a9a25cb
AndสrᎥlh๏ ⁧ <50175239+and4r1lh0@users.noreply.github.com>
2022-06-13 21:58:42 +0400

    Add files via upload

---
+++ elliptic_curve.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+ 
+def plot(a,b,p):
+    #data to be plotted
+    resx=[];resy=[]
+    for x in range(p):
+        for y in range(p):
+            if (y**2)%p==(x**3+a*x+b)%p:
+                resx.append(x%p);resy.append(y%p)
+    #preprocessing
+    result=[]
+    for el in range(len(resx)) and range(len(resy)):
+        processing = '('+str(resx[el])+', '+str(resy[el])+') '
+        result.append(processing)
+    print('Точки: ',*result, sep=",  "[1:-1])
+    #plotting
+    plt.title("Точки эллиптической кривой")
+    plt.xlabel("X");plt.ylabel("Y")
+    plt.scatter(resx, resy, color ="blue")
+    plt.show()
+  
+def add(x1,y1,x2,y2,p):
+    lam = ((y2-y1)*pow(x2-x1,-1,p))%p
+    x3 = (lam**2-x1-x2)%p
+    y3 = (lam*(x1-x3)-y1)%p
+    return x3%p,y3%p
+
+def mul(x1,y1,a,p):
+    lam = ((3*x1**2+a)*(pow(2*y1,-1,p)))%p
+    x3 = (lam**2-2*x1)%p
+    y3 = (lam*(x1-x3)-y1)%p
+    return x3%p,y3%p
+
+#параметры кривой
+a=4
+b=7
+p=11
+
+plot(a,b,p) #построение графика кривой
+
+#параметры точек
+x1=2
+y1=10
+x2=6#=x1
+y2=7#=y2
+
+#if x1!=x2:
+#    print(add(x1,y1,x2,y2,p)) #сложение точек
+#elif y1==y2:
+#    print(mul(x1,y1,a,p)) #удвоение точек
+#else:
+#    print('особая точка')
